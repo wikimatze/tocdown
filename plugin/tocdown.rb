fd57baea9cc0707504eee363827e3081d84212c1
Sebastian Günther <sebastian.guenther@vub.ac.be>
2011-12-25 12:03:46 +0100

    Fixed matching of heading syntax "### HEADING ###"

--- plugin/tocdown.rb
+++ plugin/tocdown.rb
@@ -1,7 +1,8 @@
-headings = IO.readlines(ARGV[1]).collect do |line|
-  line.match(/^[#]{1,4}/) ? ("  " * ($&.length - 1) + $'.strip) : nil
+headings = IO.readlines(ARGV[0]).collect do |line|
+ line.match(/^[#]{1,4}/) ? ("  " * ($&.length - 1) + $'.strip) : nil
 end
 
-headings.compact!.delete_if { |line| line.match /\s*#/ }
+headings.compact!.delete_if { |line| line.match /^\s*#/ }
+headings.map { |line| line.gsub!('#', '') }
 
-File.open(ARGV[0], 'w') { |file| file.write(headings.join ("\n")) }
+File.open(ARGV[1], "w") { |file| file.write(headings.join ("\n")) }
